c0e09bd53a691c3a13b69f7a3f278d80e1cae6ee
Mustapha Kassimi <mustapha_kassimi@um5.ac.ma>
2026-04-08 01:49:48 +0100

    medical microservices project

--- consultation-service/src/test/java/fr/fsr/eda/consultationservice/ConsultationServiceApplicationTests.java
+++ consultation-service/src/test/java/fr/fsr/eda/consultationservice/ConsultationServiceApplicationTests.java
@@ -2,8 +2,10 @@ package fr.fsr.eda.consultationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
-@SpringBootTest
+@SpringBootTest(properties = { "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}" })
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:0" })
 class ConsultationServiceApplicationTests {
 
 	@Test
